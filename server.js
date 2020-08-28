f0e24c042ee23704d53639b8c716ab342dcc6d80
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-08-28 19:04:50 -0400

    Added routes to return the correct html pages for urls

--- server.js
+++ server.js
@@ -12,5 +12,22 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
 // Routes
+app.get('/', (req, res) => {
+    // return index.html file
+    res.sendFile(path.join(__dirname, "public/index.html"));
+    console.log("sent index")
+});
 
+app.get('/notes', (req, res) => {
+    // return notes.html file
+    console.log("sent notes.html");
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 
