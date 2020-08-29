27075386860301515e726480b640f0104454e450
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-08-29 18:34:28 -0400

    Add functionality to update db file with new note

--- server.js
+++ server.js
@@ -2,25 +2,28 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+const routes = require('./routes');
+const db = require('./db/db.json');
 
 // Sets up Express app and port
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4001;
 
 // Sets up express app to handle data parsing (applies middleware)
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+// To access static files in the public folder
+app.use(express.static('public'));
 
-// Routes
+// ___________Routes ___________
 app.get('/', (req, res) => {
     // return index.html file
     res.sendFile(path.join(__dirname, "public/index.html"));
-    console.log("sent index")
 });
 
 app.get('/notes', (req, res) => {
     // return notes.html file
-    console.log("sent notes.html");
     res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
@@ -28,14 +31,31 @@ app.get('/api/notes', (req, res) => {
     // read db.json file and return all saved notes as JSON
     fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err, data) => {
         if (err) throw err;
-        return res.json(data);
-    })
+        return res.send(data);
+    });
+
 });
 
+// recieves a new note to save on the request body, add it to the db.json file and return the new note to the client
+app.post('/api/notes', (req, res) => {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    // Need to add a unique id number to each note when it is saved.
+    req.body.id = uuidv4();
+
+    db.push(req.body);
+
+    fs.writeFile('./db/db.json', JSON.stringify(db), {encoding: 'utf8'}, (err) => {
+        if (err) throw err;
+    });
+
+    res.send("success!");
+
+});
 
 // Starts the server to begin listening
-// =============================================================
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
 
+module.exports = routes;
