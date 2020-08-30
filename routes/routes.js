226978fdc3c7d5a831b6ed068a7ccdf4e3f77c30
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-08-30 18:16:45 -0400

    fixed get route for /notes

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 const db = require('../db/db.json');
 
@@ -10,10 +11,12 @@ router.get('/', (req, res) => {
     // return index.html file
     res.sendFile("./public/index.html");
 });
-
+// ------------------
+// Why did this one not work like the one above?
 router.get('/notes', (req, res) => {
     // return notes.html file
-    res.sendFile("./public/notes.html");
+    console.log(path.resolve(__dirname, "./../public"));
+    res.sendFile("/notes.html", { root: path.resolve(__dirname, "./../public") });
 });
 
 router.get('/api/notes', (req, res) => {
@@ -26,11 +29,9 @@ router.get('/api/notes', (req, res) => {
 
 // recieves a new note to save on the request body, add it to the db.json file and return the new note to the client
 router.post('/api/notes', (req, res) => {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-    // Need to add a unique id number to each note when it is saved.
+    // Add a unique id number to each note when it is saved.
     req.body.id = uuidv4();
-
+    // Push the new object from the req (+id) to db
     db.push(req.body);
 
     fs.writeFile('./db/db.json', JSON.stringify(db), {encoding: 'utf8'}, (err) => {
