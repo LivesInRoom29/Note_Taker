d3cd3ee599b07fb48d3feaaf6b1a836777316e78
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-09-05 23:00:42 -0400

    use next(err) to catch errors, according to docs

--- routes/routes.js
+++ routes/routes.js
@@ -18,21 +18,23 @@ router.get('/api/notes', (req, res) => {
 });
 
 // recieves a new note to save on the request body, add it to the db.json file and return the new note to the client
-router.post('/api/notes', (req, res) => {
+router.post('/api/notes', (req, res, next) => {
     // Add a unique id number to each note when it is saved.
     req.body.id = uuidv4();
     // Push the new object from the req (+id) to db
     db.push(req.body);
 
     fs.writeFile('./db/db.json', JSON.stringify(db), {encoding: 'utf8'}, (err) => {
-        if (err) throw err;
+        if (err) {
+            next(err); // Pass errors to Express
+        } else {
+            res.send('Success!');
+        }
     });
-
-    res.send('Success!');
 });
 
 // delete a note by id#
-router.delete('/api/notes/:id', (req, res) => {
+router.delete('/api/notes/:id', (req, res, next) => {
 
     idToDelete = req.params.id;
 
@@ -43,13 +45,17 @@ router.delete('/api/notes/:id', (req, res) => {
     if (found) {
         // Use filter to make newDB that includes all notes except for the one with the id
         const newDB = db.filter(note => note.id !== idToDelete);
+        console.log(newDB);
 
         // Write the newDB data to the db.json file to update the array of notes
         fs.writeFile('./db/db.json', JSON.stringify(newDB), {encoding: 'utf8'}, (err) => {
-            if (err) throw err;
+            if (err) {
+                next(err) // Pass errors to Express.
+            } else {
+                res.send(`Note with id of ${idToDelete} was deleted!`);
+            }
         });
 
-        res.send(`Note with id of ${idToDelete} was deleted!`);
     } else {
         // if there's not note with that id, return error status
         res.status(400).json({ msg: `No note with the ID of ${req.params.id} exists.`});
